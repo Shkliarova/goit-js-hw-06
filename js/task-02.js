60367a27e38b2cffda2ad031f214f56e80a5a684
Shkliarova <shklyarovaalisa@gmail.com>
2023-09-03 22:29:04 +0300

    fix task 2

--- js/task-02.js
+++ js/task-02.js
@@ -9,12 +9,12 @@ const ingredients = [
 
 const list = document.querySelector('#ingredients');
 
-const ingredient = ingredients.forEach(element =>{
+const ingredient = ingredients.map(element =>{
   const item = document.createElement('li');
   item.textContent = element;
-  item.classList = ".item";
-  list.append(item);
+  item.classList.add("item");
+  return item;
 });
 
-
+list.append(...ingredient);
 console.log(list);
